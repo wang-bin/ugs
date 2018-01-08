4018459b69bf85bab4370eee9e77709c65e06d46
wang-bin <wbsecg1@gmail.com>
2018-01-09 00:10:44 +0800

    mpsc fifo improvement

--- base/mpsc_fifo.h
+++ base/mpsc_fifo.h
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2017 WangBin <wbsecg1 at gmail.com>
+ * Copyright (c) 2017-2018 WangBin <wbsecg1 at gmail.com>
+ * MIT License
  * Lock Free MPSC FIFO
+ * https://github.com/wang-bin/lock_free
  */
 #include <atomic>
 #include <utility>
@@ -8,43 +10,53 @@
 template<typename T>
 class mpsc_fifo {
 public:
-    mpsc_fifo() { tail_.store(head_); }
+    mpsc_fifo() { in_.store(out_); }
+
     ~mpsc_fifo() {
-        while (node* h = head_) {
-            head_ = h->next.load();
+        while (node* h = out_) {
+            out_ = h->next.load();
             delete h;
         }
     }
-    void push(T&& v) {
-        node *n = new node();
-        n->v = std::move(v);
-#if 0
-        node* t = tail_.load(std::memory_order_relaxed);
-        while (!tail_.compare_exchange_weak(t, n, std::memory_order_acq_rel, std::memory_order_relaxed)) {}
-        t->next.store(n, std::memory_order_release);
-#else
-        tail_.exchange(n, std::memory_order_acq_rel)->next.store(n, std::memory_order_release);
-#endif
+
+    void clear() { while(pop()) {}} // in consumer thread
+
+    template<typename... Args>
+    void push(Args&&... args) {
+        push({std::forward<Args>(args)...});
     }
+
+    void push(T&& v) {
+        node *n = new node{std::move(v)};
+#if 0
+        node* t = in_.load(std::memory_order_relaxed);
+        while (!in_.compare_exchange_weak(t, n, std::memory_order_acq_rel, std::memory_order_relaxed)) {}
+#else
+        node* t = in_.exchange(n, std::memory_order_acq_rel);
+#endif
+     
+        t->next.store(n, std::memory_order_release);
+    }
+
     bool pop(T* v = nullptr) {
         // will check next.load() later, also next.store() in push() must be after exchange, so relaxed is enough
-        if (head_ == tail_.load(std::memory_order_relaxed)) //if (!head->next) // not completely write to head->next (tail->next), next is not null but invalid
+        if (out_ == in_.load(std::memory_order_relaxed)) //if (!head->next) // not completely write to head->next (tail->next), next is not null but invalid
             return false;
-        node *n = head_->next.load(std::memory_order_relaxed);
+        node *n = out_->next.load(std::memory_order_relaxed);
         if (!n)
             return false;
         if (v)
             *v = std::move(n->v);
-        delete head_;
-        head_ = n;
+        delete out_;
+        out_ = n;
         return true;
     }
 private:
     struct node {
-        std::atomic<node*> next;
         T v;
+        std::atomic<node*> next;
     };
-    node *head_ = new node(); // pop_at_
-    std::atomic<node*> tail_; // push_at_ // can not use tail_{head_} because atomic ctor with desired value MUST be constexpr (error in g++4.8)
-};
 
+    node *out_ = new node();
+    std::atomic<node*> in_; // can not use in_{out_} because atomic ctor with desired value MUST be constexpr (error in g++4.8 iff use template)
+};
